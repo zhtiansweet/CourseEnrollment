9c6862e67c2c8a59d934b74d5b88d00d655fa3be
Tian Zhang <tianzhang@Tians-MacBook-Pro-2.local>
2015-05-26 16:04:54 -0500

    update

---
+++ README.txt
@@ -0,0 +1,84 @@
+Course Enrollment System
+
+IDE: IntelliJ
+If you have problems on opening the whole project, please open a new project in your IDE and run the 4 .java files in src folder.
+
+The class CourseEnroll implements interface EnrollActivity and contains a main function. You could add some courses and students and modify the enrollment activities to play with the system.
+
+The subclasses UndergradStudent and GradStudent inherit from superclass Student. And I also designed a class Course.
+
+=====Enrollment Rules=====
+
+* Each course has three status: open, wl(waiting list) and close. When initialized, a course's status will be set as "open".
+* Each course has two student lists. One is for students that have been enrolled successfully, and the other is for students on the waiting list.
+* When a student adds a course, (s)he will be prior added to the first list. When it is full, the course status will be set as "wl", and students will be added to the waiting list then. And when the waiting list is full, the course status will be set as "close", and students couldn't select this course anymore.
+* When a student delete a course, (s)he will be deleted from the lists. And if there appears a position on the "enrolled" list, the first student on the waiting list will be enrolled. Besides, the course status will change correspondingly.
+* A student could select as many as 4 courses. Duplicated courses couldn't be selected.
+* Undergraduate students could only select courses with id under 400, and Graduate students could only select courses with id over 300.
+
+=====Class & Interface Details=====
+The fields and methods of each class and interface are as below.
+
+interface EntollActivity methods:
+* void add(Course cou, UndergradStudent stu)
+* void add(Course cou, GradStudent stu)
+* void delete
+
+class CourseEnroll implements EnrollActivity methods:
+* void add(Course cou, UndergradStudent stu)
+  * Call UndergradStudent.add, Course.addStudent, Student.printCourseList(), Course.printStudentList()
+* void add(Course cou, GradStudent stu)
+  * Call GradStudent.add, Course.addStudent, Student.printCourseList(), Course.printStudentList()
+* void delete
+  * Call Student.deleteCourse(cou), Course.deleteStudent(stu), Student.printCourseList(), Course.printStudentList();
+* main function
+
+class Student fields (all private):
+* static final int MAX_COURSE=4 (A student could select as many as 4 courses per quarter)
+* int id (Unique for each student)
+* String degree ("undergrad" or "grad")
+* String name
+* ArrayList<Course> enrollList (The courses this student has been enrolled into)
+* ArrayList<Course> waitList (The courses whose waiting lists this student is on)
+
+
+class Student methods (all public):
+* Constructor
+* Setters & Getters
+* void printCourseList (Print out the enrollList & waitList)
+* boolean addCourse (Add a course to this student's enrollList or waitList)
+* void deleteCourse (Delete a course from this student's enrollList or waitList)
+
+class UndergradStudent extends Student extra field:  
+* static final String degree = "undergrad"
+
+class UndergradStudent extends Student extra method:
+* boolean add (Undergraduate students could only select courses with id under 400)
+    * Call boolean addCourse
+
+class GradStudent extends Student extra field:
+* static final String degree = "grad"
+
+class GradStudent extends Student extra method:
+* boolean add (Graduate students could only select courses with id over 300)
+    * Call boolean addCourse
+
+class Course fields (all private):
+
+* int id (Unique for each course)
+* String name
+* String instructor
+* int unit
+* int capacity
+* int wlCapacity
+* String state ("open", "wl", "close")
+* ArrayList<Student> studentList (Students that has been enrolled)
+* ArrayList<Student> studentWl (Students on waiting list)
+
+class Course methods (all public):
+
+* Constructor
+* Setters & Getters
+* void printStudentList (Print out studentList & studentWl)
+* void addStudent (Add a student to this course's studentList or studentWl)
+* void deleteStudent (Delete a student from this course's studentList or studentWl)
--- src/CourseEnroll.java
+++ src/CourseEnroll.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by tianzhang on 5/23/15.
  */
@@ -10,16 +13,14 @@ class UndergradStudent extends Student{
     }
 
     @Override
-    public String getDegree(){return degree;}
-
-    public boolean add(Course cou){
+    public boolean addCourse(Course cou){
         //Undergraduate students could only select courses under 400
         if (cou.getId()>=400){
             System.out.println("Couldn't select course on this level");
             return false;
         }
         else{
-            if(!addCourse(cou)) return false;       //Failed to add the course
+            if(!super.addCourse(cou)) return false;       //Failed to add the course
         }
         return true;
     }
@@ -34,16 +35,14 @@ class GradStudent extends Student{
     }
 
     @Override
-    public String getDegree(){return degree;}
-
-    public boolean add(Course cou){
+    public boolean addCourse(Course cou){
         //Graduate students could only select courses above 300
         if (cou.getId()<300){
             System.out.println("Couldn't select course on this level");
             return false;
         }
         else{
-            if(!addCourse(cou)) return false;       //Failed to select the course
+            if(!super.addCourse(cou)) return false;       //Failed to select the course
         }
         return true;
     }
@@ -54,20 +53,26 @@ public class CourseEnroll implements EnrollActivity{
 
     //Implement methods in EnrollActivity
     public void add(Course cou, UndergradStudent stu){
-        if(stu.add(cou)) cou.addStudent(stu);       //If stu selected cou successfully, add the stu to cou's list
+        System.out.println("Undergraduate student "+stu.getId()+" adds Course "+cou.getId());
+        System.out.println();
+        if(stu.addCourse(cou)) cou.addStudent(stu);       //If stu selected cou successfully, add the stu to cou's list
         stu.printCourseList();
         cou.printStudentList();
         System.out.println("===============");
     }
 
     public void add(Course cou, GradStudent stu){
-        if(stu.add(cou)) cou.addStudent(stu);
+        System.out.println("Graduate student "+stu.getId()+" adds Course "+cou.getId());
+        System.out.println();
+        if(stu.addCourse(cou)) cou.addStudent(stu);
         stu.printCourseList();
         cou.printStudentList();
         System.out.println("===============");
     }
 
     public void delete(Course cou, Student stu){
+        System.out.println("Student "+stu.getId()+" deletes Course "+cou.getId());
+        System.out.println();
         stu.deleteCourse(cou);
         cou.deleteStudent(stu);
         stu.printCourseList();
@@ -80,50 +85,51 @@ public class CourseEnroll implements EnrollActivity{
         CourseEnroll ce = new CourseEnroll();       //Initialize a CourseEnroll instance to implement methods above
 
         //Initialize some courses
-        Course c0 = new Course(100, "Intro to Computer Science", "Alice", 5, 4);
-        Course c1 = new Course(211, "Data Structures", "Bob", 3, 5);
-        Course c2 = new Course(349, "Machine Learning", "Cindy", 2, 3);
-        Course c3 = new Course(336, "Algorithms", "Quiz", 4, 1);
-        Course c4 = new Course(343, "Operating System", "Ruby", 3, 2);
-        Course c5 = new Course(432, "Computer Vision", "David", 5, 2);
-        Course c6 = new Course(510, "Data Mining", "Emma", 4, 3);
+        ArrayList<Course> courses = new ArrayList<>();
+        courses.add(new Course(100, "Intro to Computer Science", "Alice", 2, 4));
+        courses.add(new Course(211, "Data Structures", "Bob", 3, 1));
+        courses.add(new Course(349, "Machine Learning", "Cindy", 2, 3));
+        courses.add(new Course(336, "Algorithms", "Quiz", 3, 1));
+        courses.add(new Course(343, "Operating System", "Ruby", 3, 2));
+        courses.add(new Course(432, "Computer Vision", "David", 2, 2));
+        courses.add(new Course(510, "Data Mining", "Emma", 3, 4));
 
         //Initialize some students
-        UndergradStudent s0 = new UndergradStudent(12300, "Frank");
-        UndergradStudent s1 = new UndergradStudent(12301, "Geek");
-        GradStudent s2 = new GradStudent(12302, "Harry");
-        UndergradStudent s3 = new UndergradStudent(12303, "Ivy");
-        GradStudent s4 = new GradStudent(12304, "Jack");
-        GradStudent s5 = new GradStudent(12305, "Katie");
-        GradStudent s6 = new GradStudent(12306, "Lana");
-        UndergradStudent s7 = new UndergradStudent(12307, "Mike");
-        UndergradStudent s8 = new UndergradStudent(12308, "Nancy");
-        GradStudent s9 = new GradStudent(12309, "Owen");
-        UndergradStudent s10 = new UndergradStudent(12310, "Peter");
+        ArrayList<UndergradStudent> undergradStudents = new ArrayList<>();
+        undergradStudents.add(new UndergradStudent(12300, "Frank"));
+        undergradStudents.add(new UndergradStudent(12301, "Geek"));
+        undergradStudents.add(new UndergradStudent(12303, "Ivy"));
+        undergradStudents.add(new UndergradStudent(12307, "Mike"));
+        undergradStudents.add(new UndergradStudent(12308, "Nancy"));
+        undergradStudents.add(new UndergradStudent(12310, "Peter"));
 
-        //Enrollment activities
-        ce.add(c1, s1);
-        ce.add(c1, s5);
-        ce.add(c1, s8);
-        ce.add(c5, s2);
-        ce.add(c2, s2);
-        ce.delete(c1, s5);
-        ce.add(c3, s2);
-        ce.add(c6, s2);
-        ce.add(c2, s10);
-        ce.add(c2, s6);
-        ce.add(c2, s8);
-        ce.add(c2, s3);
-        ce.add(c0, s0);
-        ce.add(c4, s4);
-        ce.add(c3, s7);
-        ce.add(c3, s9);
-        ce.delete(c2, s8);
-        ce.add(c4, s8);
-        ce.delete(c3, s9);
-        ce.add(c3, s2);
-        ce.add(c2, s4);
-        ce.add(c2, s5);
-        ce.add(c3, s2);
+        ArrayList<GradStudent> gradStudents = new ArrayList<>();
+        gradStudents.add(new GradStudent(12302, "Harry"));
+        gradStudents.add(new GradStudent(12304, "Jack"));
+        gradStudents.add(new GradStudent(12305, "Katie"));
+        gradStudents.add(new GradStudent(12306, "Lana"));
+        gradStudents.add(new GradStudent(12309, "Owen"));
+
+        //Random enrollment activities
+        for (int i=0;i<15;i++){
+            if (new Random().nextDouble()>=0.5){
+                ce.add(courses.get(randInt(0, courses.size()-1)), undergradStudents.get(randInt(0, undergradStudents.size()-1)));
+            }
+            if (new Random().nextDouble()>=0.5){
+                ce.add(courses.get(randInt(0, courses.size()-1)), gradStudents.get(randInt(0, gradStudents.size()-1)));
+            }
+            if (new Random().nextDouble()>=0.5){
+                ce.delete(courses.get(randInt(0, courses.size() - 1)), gradStudents.get(randInt(0, gradStudents.size() - 1)));
+            }
+            if (new Random().nextDouble()>=0.5){
+                ce.delete(courses.get(randInt(0, courses.size() - 1)), undergradStudents.get(randInt(0, undergradStudents.size() - 1)));
+            }
+        }
     }
+
+    public static int randInt(int min, int max){
+        Random rm = new Random();
+        return rm.nextInt(max-min+1)+min;
+    }
+
 }
